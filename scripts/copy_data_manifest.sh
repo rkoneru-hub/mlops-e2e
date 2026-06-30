27fc55a9ff936044d49e6d6bf6750c8178573fc0
ravikanth k <ravikanthkoneru@hotmail.com>
2026-06-30 19:37:39 +0100

    Debug SourceData manifest

--- scripts/copy_data_manifest.sh
+++ scripts/copy_data_manifest.sh
@@ -4,8 +4,20 @@ set -e
 
 # Copy the Data Manifest file to the main folder
 
-echo "Data Manifest:"
+# echo "Data Manifest:"
+# cat ${CODEBUILD_SRC_DIR_SourceDataOutput}/manifest.json
+
+# echo "Copying Data Manifest to main folder"
+# cat ${CODEBUILD_SRC_DIR_SourceDataOutput}/manifest.json > ./dataManifest.json
+
+echo "===== Files in SourceDataOutput ====="
+find ${CODEBUILD_SRC_DIR_SourceDataOutput} -type f
+
+echo "===== Directory Structure ====="
+ls -R ${CODEBUILD_SRC_DIR_SourceDataOutput}
+
+echo "===== Data Manifest ====="
 cat ${CODEBUILD_SRC_DIR_SourceDataOutput}/manifest.json
 
-echo "Copying Data Manifest to main folder"
-cat ${CODEBUILD_SRC_DIR_SourceDataOutput}/manifest.json > ./dataManifest.json
+echo "Copying Data Manifest"
+cp ${CODEBUILD_SRC_DIR_SourceDataOutput}/manifest.json ./dataManifest.json
